b3b341903e42b79c5c17cf524ab8d641d45aac9c
karma <karmarankartik@gmail.com>
2026-09-22 19:47:49 +0530

    swagger addition

--- src/main/java/com/ubiqedge/billing_software/dto/UpdateWaterMeterRequest.java
+++ src/main/java/com/ubiqedge/billing_software/dto/UpdateWaterMeterRequest.java
@@ -5,7 +5,6 @@ import java.util.UUID;
 
 public record UpdateWaterMeterRequest(
         String meterNumber,
-        UUID billingPlanId,
-        BigDecimal currentTotalReading
+        UUID billingPlanId
 ) {
 }
